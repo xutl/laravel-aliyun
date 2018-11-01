b2993f54717ab0e1037ae52617ec585c63f10f91
XuTongle <xutongle@gmail.com>
2018-11-01 12:14:24 +0800

    配置文件

--- config/aliyun.php
+++ config/aliyun.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
+    'access_id' => env('ALIYUN_ACCESS_ID'),
+    'access_key' => env('ALIYUN_ACCESS_KEY'),
 
+    'cdn' => [
+        'access_id' => env('ALIYUN_ACCESS_ID'),
+        'access_key' => env('ALIYUN_ACCESS_KEY'),
+    ],
 ];
